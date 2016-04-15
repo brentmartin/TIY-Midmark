7c1aa649bc837110ab5ac0190fcf2620db0fb64b
Brent Martin <bmart35@gmail.com>
2016-04-15 15:00:31 -0500

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+5.times do |n|
+  user = User.create!(name: Faker::Name.name,
+             location: Faker::Address.city + ", " + Faker::Address.state_abbr,
+             username: Faker::Internet.user_name,
+             password_digest: "password")
+
+  4.times do
+  mark = Mark.create!(message: Faker::Company.catch_phrase + " " + Faker::Company.bs, user_id: user.id)
+  end
+end
